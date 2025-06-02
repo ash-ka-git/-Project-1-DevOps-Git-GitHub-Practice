3eee4f9de3f31d40a9c25cf0bc18344e98d00f47
Ashish korhalkar <ashkorhalkar@gmail.com>
2025-06-02 23:45:56 +0530

    changes done in app.py file

--- service-backend/app.py
+++ service-backend/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Backend!"
+    return "Hello from Backend!+ extra part added for check"
 
 if __name__ == '__main__':
     app.run(debug=True)
